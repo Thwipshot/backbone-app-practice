2c731e80fee107ada85ef75f470745a7b8ca7c54
Thwipshot <ethumpert@gmail.com>
2016-07-24 18:29:32 -0500

    first commit of the example contact manager app there were typo errors I made in the code but I debugged them before committing

--- project/scripts/main.js
+++ project/scripts/main.js
@@ -1,1 +1,43 @@
+var App = {
+  Models: {},
+  Collections: {},
+  Contacts: null
+};
 
+$(function() { // Run this code when the DOM is ready
+  window.tom = new App.Models.Contact({
+    firstName: 'Thomas',
+    lastName: 'Hunter',
+    phoneNumber: '9895135499',
+    email: 'me@thomashunter.name'
+  });
+
+  App.Contacts = new
+  App.Collections.Contact();
+  App.Contacts.add(window.tom);
+
+  App.Contacts.add({
+    firstName: 'Rupert',
+    lastName: 'Styx',
+    phoneNumber: '9895551212',
+    email: 'rupertstyx@example.com'
+  });
+
+  App.Contacts.add({});
+
+  var contactListing = '';
+  App.Contacts.each(function(contact) {
+    contactListing += "<div>" +
+    contact.get('firstName') + " " +
+    contact.get('lastName') + " ";
+
+    if (contact.isValid()) {
+      contactListing += "(valid)";
+    } else {
+      contactListing += "(invalid)";
+    }
+
+    contactListing += "</div>";
+  });
+  $('#display').html(contactListing);
+});
